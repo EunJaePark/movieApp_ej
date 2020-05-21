ea71e96da995a06532c861e2ecd2c3a740db2b57
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-05-21 19:50:26 +0900

    테스트용 components, view 생성

---
+++ src/components/MainConts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      MainPage에 연결되는 컴포넌트 MainConts이다.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      Main page
+      <MainConts></MainConts>
+  </div>
+</template>
+
+<script>
+import MainConts from '../components/MainConts';
+
+export default {
+  components: {
+    MainConts,
+  }
+}
+</script>
+
+<style>
+
+</style>
