c7100ff35db2d2694baefb229d6f7aad3fadf8f1
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-05-30 18:01:52 +0900

    스틸컷 v-for문 돌려서 불러오기 성공!!!!!🤩

--- src/components/InformConts.vue
+++ src/components/InformConts.vue
@@ -3,7 +3,7 @@
       <div class="topData clear">
         <img 
           class="poster"                    
-          v-bind:src="editURL(clickMovieData.posters)" 
+          v-bind:src="posterURL(clickMovieData.posters)" 
           v-bind:alt="textEdit(clickMovieData.title)"
         />
         <div class="summaryBox">
@@ -56,9 +56,13 @@
             {{ storyEdit(clickMovieData.plots.plot[0].plotText) }}
           </p>
           
+            {{ photoIndex(clickMovieData.stlls) }}
           <div class="stills">
             <P style="width:100%; overflow:scroll;">스틸컷url: {{ clickMovieData.stlls }}</P>
-            <img :src="editURL(clickMovieData.stlls)" />
+            <div  v-for="(photo, imgIndex) in photoIndex(clickMovieData.stlls)" :key="imgIndex" >
+              <img :src="stllImgURL(clickMovieData.stlls, imgIndex)" alt="" />
+              {{ imgIndex }}
+            </div>
           </div>
 
           <p v-if="clickMovieData.actors.actor[0].actorNm !== ''">배우: 
@@ -72,7 +76,6 @@
           <p><a v-bind:href="clickMovieData.kmdbUrl" target="blanket">상세정보</a></p>
       </div><!--.detailBox-->
       
-      
       <br/>
       <!-- <div style="border:2px solid red;">클릭한 id 영화 데이터: {{ clickMovieData }}</div> -->
       
@@ -81,6 +84,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      imgIndex: 0,
+    }
+  },
   computed: {
     clickMovieData() {
       return this.$store.state.clickMovie;
@@ -91,15 +99,15 @@ export default {
       // 검색어를 나타내는 !HE, !HS글자 삭제.
       return text.replace(/!HE|!HS/g, '');
     },
-    editURL(url) {            
-        if(url === '') {   // url이 없을 경우
-          // poster url주소가 없을 경우 빈 이미지 추출.
-          return 'http://placehold.it/144x86';
-        }else if(url.indexOf('|') === -1) {   // url이 1개일 경우
-          return url;
-        }else if(url.indexOf('|')) {   // url이 2개 이상일 경우  
-          return url.substring(0, url.indexOf('|')); 
-        } 
+    posterURL(url) {            
+      if(url === '') {   // url이 없을 경우
+        // poster url주소가 없을 경우 빈 이미지 추출.
+        return 'http://placehold.it/213x303';
+      }else if(url.indexOf('|') === -1) {   // url이 1개일 경우
+        return url;
+      }else if(url.indexOf('|')) {   // url이 2개 이상일 경우  
+        return url.substring(0, url.indexOf('|')); 
+      } 
     },
     // 줄거리 중 제일 앞 문장만 추출.
     sloganEdit(story) {
@@ -108,6 +116,29 @@ export default {
     // 줄거리 중 제일 앞 문장 제외한 나머지 모든 줄거리 추출.
     storyEdit(story) {
       return story.substring(story.indexOf('!')+1, story.length)
+    },
+    // 스틸컷 img 개수 세는 함수.
+    photoIndex(photoUrl) {
+      if(photoUrl === '') {
+        this.imgIndex = 0;
+        return 0;
+      } else if(photoUrl.indexOf('|') === -1) {
+        this.imgIndex = 1;
+        return 1;
+      } else if(photoUrl.indexOf('|')) { // http의 개수에 따라 v-for이용해 이미지 태그 생성.
+        this.imgIndex = photoUrl.match(/http/g).length;
+        return photoUrl.match(/http/g).length;
+      }
+    },
+    stllImgURL(url, index) {            
+      if(url === '') {   // url이 없을 경우
+        // poster url주소가 없을 경우 빈 이미지 추출.
+        return 'http://placehold.it/144x86';
+      } else if(url.indexOf('|') === -1) {   // url이 1개일 경우
+        return url;
+      } else if(url.indexOf('|')) {   // url이 2개 이상일 경우  
+        return url.split('|')[index];  // split를 이용해 '|'를 기준으로 url을 나눔. 그 뒤에 각 index에 맞는 사진url을 return.
+      } 
     },
   }
 }
